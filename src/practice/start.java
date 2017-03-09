a70d78d8a88b192c136c5d395d3aa26023ca2f1e
jacob <redphinx93@gmail.com>
2017-03-08 22:29:46 -0600

    UseCase
    Added Use case folder

--- src/practice/start.java
+++ src/practice/start.java
@@ -158,4 +158,21 @@ public class start {
 			return -1;
 		}
 	}
+	
+	/**
+	 * mysql statement execution method. This method executes mysql commands given a query.  
+	 * @param	stmt	Is the current statement and located within a database.
+	 * @param	query	Is the command wanted to be executed at given statement.
+	 * @return	rs		Returns the int of that given execution statement.
+	 */
+	public int myUpdateQuery(Statement stmt, String query){
+		try{
+			int rs = stmt.executeUpdate(query);
+			return rs;
+		}catch(Exception e){
+			System.out.println("query failed");
+			System.out.println(e);
+			return -1;
+		}
+	}
 }
